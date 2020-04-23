2cc9bb280f3edcb3d39c0c2358b92c6da1ef651f
krzysztof.surulo <krzysztof.surulo@silvermedia.pl>
2020-04-23 16:33:48 +0200

    Updates

---
+++ precache-manifest.632caa443dbaba268bb5bc59610ec448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe90a3ec502a41f95323f608cc5f99d1",
+    "url": "/lexicon2/index.html"
+  },
+  {
+    "revision": "2347ad39d26db1f48f3c",
+    "url": "/lexicon2/static/css/main.74ef625e.chunk.css"
+  },
+  {
+    "revision": "b43ee2472afe1e865b03",
+    "url": "/lexicon2/static/js/2.ad777b13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lexicon2/static/js/2.ad777b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2347ad39d26db1f48f3c",
+    "url": "/lexicon2/static/js/main.814079fa.chunk.js"
+  },
+  {
+    "revision": "36d12d3dfeef22dcd8b7",
+    "url": "/lexicon2/static/js/runtime-main.4fd3bae3.js"
+  }
+]);
